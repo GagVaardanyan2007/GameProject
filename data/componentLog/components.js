094aee6255628a5f97a67b1f13325cb9461b18d4
GagVaardanyan2007 <foxikmopzzi@gmail.com>
2025-10-07 01:43:37 +0400

    Update components.js

--- data/componentLog/components.js
+++ data/componentLog/components.js
@@ -1,30 +1,30 @@
-import App from "../../src/App";
-import LogIn from "../../src/components/sections/Log-in/LogIn";
-import Registration from "../../src/components/sections/Log-in/Registration";
-import leaderBoardAllSection from "../../src/components/sections/mainSections/leaderBoardSection/leaderBoardAllSection";
+// import App from "../../src/App";
+// import LogIn from "../../src/components/sections/Log-in/LogIn";
+// import Registration from "../../src/components/sections/Log-in/Registration";
+// import leaderBoardAllSection from "../../src/components/sections/mainSections/leaderBoardSection/leaderBoardAllSection";
 
-import { login } from "../login/login_data";
-import { register } from "../login/registration_data";
-import { leaderPage } from "../mainSection/leaderboard/leaderBoardPage";
+// import { login } from "../login/login_data";
+// import { register } from "../login/registration_data";
+// import { leaderPage } from "../mainSection/leaderboard/leaderBoardPage";
 
 
-export const component = {
-    allComponent:[
-        {
-            component:Registration(register),
-            path:'/register',
-        },
-        {
-            component:App(),
-            path:'/',
-        },
-        {
-            component:LogIn(login),
-            path:'/LogIn',
-        },
-        {
-            component:leaderBoardAllSection(leaderPage),
-            path:'/leaderboard',
-        },
-    ]
-}
+// export const component = {
+//     allComponent:[
+//         {
+//             component:Registration(register),
+//             path:'/register',
+//         },
+//         {
+//             component:App(),
+//             path:'/',
+//         },
+//         {
+//             component:LogIn(login),
+//             path:'/LogIn',
+//         },
+//         {
+//             component:leaderBoardAllSection(leaderPage),
+//             path:'/leaderboard',
+//         },
+//     ]
+// }
